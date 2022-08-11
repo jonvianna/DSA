57eee76ce7da66877e308c530a3b88a9e0b753db
Jon Vianna <101150408+jonvianna@users.noreply.github.com>
2022-08-10 22:30:27 -0300

    Add files via upload

---
+++ Aula1/hello/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace hello
+{
+    class Program
+    {
+        static double CalculoArea(double largura, double altura){
+            return largura * altura;
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Digite uma largura:");
+           double largura = Convert.ToDouble(Console.ReadLine());
+
+           Console.WriteLine("Digite uma altura:");
+           double altura = Convert.ToDouble(Console.ReadLine());
+           
+            double area = CalculoArea(largura, altura);
+            Console.Write("A area é: ");
+            Console.WriteLine(area);
+            }
+            catch (Exception excecao)
+            {
+                Console.WriteLine(excecao.Message);
+            }
+            
+       }
+    }
+}
