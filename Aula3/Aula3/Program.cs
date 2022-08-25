da55f395af6e854ca75c3cfe11e26b072887c7fa
Jon Vianna <101150408+jonvianna@users.noreply.github.com>
2022-08-24 22:23:29 -0300

    Add files via upload

---
+++ Aula3/Aula3/Program.cs
@@ -0,0 +1,48 @@
+﻿// EXERCICIO = criar uma tela de login
+
+using System;
+using System.IO;
+
+namespace Aula3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá! Bem vindo ao sistema de login");
+            Console.WriteLine("Digite 1 para logar");
+            Console.WriteLine("Digite 2 para cadastrar");
+            Console.WriteLine("Digite 3 para sair");
+            bool sair = false;
+            while(!sair)
+            {
+                int opcao = 0;
+                try
+                {
+                    opcao = int.Parse(Console.ReadLine());
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("valor inválido, tente novamente");
+                }
+
+                switch(opcao)
+                {
+                    case 1:
+                        // logar
+                        break;
+                    case 2:
+                        // cadastrar
+                        break;
+                    case 3:
+                        sair = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida! Tente novamente");
+                        break;
+                
+                }
+            }
+        }
+    }
+}
